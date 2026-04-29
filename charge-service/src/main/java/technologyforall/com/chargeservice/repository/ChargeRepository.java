200ff806f908dc991a9954bcb2b3d72d2d1fffaf
Jonathan Chidubem Godson <godsonjonathan39@gmail.com>
2026-04-29 14:43:12 +0100

    added local and dev profile

---
+++ charge-service/src/main/java/technologyforall/com/chargeservice/repository/ChargeRepository.java
@@ -0,0 +1,9 @@
+package technologyforall.com.chargeservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import technologyforall.com.chargeservice.model.Charge;
+
+@Repository
+public interface ChargeRepository extends JpaRepository<Charge,Long> {
+}
