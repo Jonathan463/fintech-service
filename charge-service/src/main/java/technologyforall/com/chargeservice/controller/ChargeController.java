c76750f2b42bdbb0de7a7e4508a2d0c0aad718a9
Jonathan Chidubem Godson <godsonjonathan39@gmail.com>
2026-05-02 16:43:58 +0100

    fixed port mismatch error and added KafkaProducerConfig for Charge service

--- charge-service/src/main/java/technologyforall/com/chargeservice/controller/ChargeController.java
+++ charge-service/src/main/java/technologyforall/com/chargeservice/controller/ChargeController.java
@@ -1,6 +1,7 @@
 package technologyforall.com.chargeservice.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,7 @@ import technologyforall.com.chargeservice.service.ChargeServiceImpl;
 
 @RestController
 @RequestMapping("/api/charges")
+@Slf4j
 @RequiredArgsConstructor
 public class ChargeController {
 
@@ -17,6 +19,7 @@ public class ChargeController {
 
     @PostMapping
     public ResponseEntity<ChargeResponse> calculateCharge(@RequestBody ChargeRequest request) {
+        log.info("got to the charge controller");
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(chargeServiceImpl.calculateAndSaveCharge(request));
