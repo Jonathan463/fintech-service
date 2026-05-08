e8b77f1bf91d0a64235b33daf68400423b2e9cbc
Jonathan Chidubem Godson <godsonjonathan39@gmail.com>
2026-05-08 22:46:52 +0100

    added @EnabledKafka to consumer classes to make consumers detectable by Kafka UI

--- charge-service/src/main/java/technologyforall/com/chargeservice/config/KafkaConsumerConfig.java
+++ charge-service/src/main/java/technologyforall/com/chargeservice/config/KafkaConsumerConfig.java
@@ -5,6 +5,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
@@ -17,6 +18,7 @@ import org.springframework.util.backoff.FixedBackOff;
 import java.util.HashMap;
 import java.util.Map;
 
+@EnableKafka
 @Configuration
 public class KafkaConsumerConfig {
 
