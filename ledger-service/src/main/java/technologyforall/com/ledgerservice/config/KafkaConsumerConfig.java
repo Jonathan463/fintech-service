f49af2a620d1a43c94ee7187325eb101202c0486
Jonathan Chidubem Godson <godsonjonathan39@gmail.com>
2026-05-15 00:00:53 +0100

    fixed serialization error in the consumer service charge-service,ledger-service,notification-service,transfer-service

--- ledger-service/src/main/java/technologyforall/com/ledgerservice/config/KafkaConsumerConfig.java
+++ ledger-service/src/main/java/technologyforall/com/ledgerservice/config/KafkaConsumerConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import technologyforall.com.ledgerservice.event.ChargeCalculatedEvent;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -35,7 +36,7 @@ public class KafkaConsumerConfig {
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
         props.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
-        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, Object.class);
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, ChargeCalculatedEvent.class);
         return new DefaultKafkaConsumerFactory<>(props);
     }
 
