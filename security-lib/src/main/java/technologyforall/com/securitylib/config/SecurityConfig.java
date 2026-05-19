7423bac8789c0c1ae394ffccc86e04be700735ea
Jonathan Chidubem Godson <godsonjonathan39@gmail.com>
2026-05-19 09:54:27 +0100

    Fixed zero trust implementation with Keycloack

--- security-lib/src/main/java/technologyforall/com/securitylib/config/SecurityConfig.java
+++ security-lib/src/main/java/technologyforall/com/securitylib/config/SecurityConfig.java
@@ -1,37 +1,19 @@
 package technologyforall.com.securitylib.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.security.oauth2.server.resource.autoconfigure.servlet.OAuth2ResourceServerAutoConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import technologyforall.com.securitylib.converter.KeycloakJwtAuthConverter;
 
-@Configuration
-@EnableWebSecurity
+@AutoConfiguration(before = OAuth2ResourceServerAutoConfiguration.class)
 @EnableMethodSecurity
 public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(s ->
-                        s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/actuator/health").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
-                );
-        return http.build();
-    }
 
     @Bean
     public JwtAuthenticationConverter jwtAuthenticationConverter() {
